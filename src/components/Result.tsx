4a282c1d1a72a5eb7b18661dca20444e56069f9c
Rafael Diorato <diorato26@gmail.com>
2024-10-11 02:46:20 -0500

    Finalizado a calculadora de IMC

---
+++ src/components/Result.tsx
@@ -0,0 +1,9 @@
+function Result() {
+    return (
+      <p className="text-center text-neutral-400 text-xl">
+        Saiba agora se está no seu peso ideal.
+      </p>
+    );
+}
+
+export default Result;
